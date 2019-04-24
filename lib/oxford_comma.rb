4efe213fd975c15c33297143ab48472c4e6eb7d2
Fabian Fabro <fabrofabian@gmail.com>
2019-04-24 06:56:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  if array.length <= 1
+    result = array.join
+    return result
+  elsif array.length == 2
+    result = array.join(" and ")
+    return result
+  elsif array.length >= 3
+    count = 0
+      while count <= array.length
+        break if count == array.length - 1
+        result = array.join(", ")
+        count += 1
+      end
+      result = array.join(" and #{array[-1]}")
+      return result
+  end
 
+  #result = array.join(" and")
 end
